76b047bbecb9811163a10ba253c6b64a15ab80e8
moontai0724 <moontai0724@gmail.com>
2023-02-26 21:15:23 +0800

    feat: person service

---
+++ src/services/person.service.ts
@@ -0,0 +1,37 @@
+export default class PersonService implements Service<RawPerson> {
+  protected readonly TABLE_NAME = "Persons";
+
+  public spreadsheet;
+  public sheet!: GoogleAppsScript.Spreadsheet.Sheet;
+
+  public constructor() {
+    this.spreadsheet = SpreadsheetApp.getActiveSpreadsheet();
+    const sheet = this.spreadsheet.getSheetByName(this.TABLE_NAME);
+    if (!sheet) throw new Error("Sheet not found");
+  }
+
+  public getAll(): RawPerson[] {
+    const data = this.sheet.getDataRange().getValues();
+    const items = data.slice(1).map(this.buildData);
+
+    return items;
+  }
+
+  public get(id: string): RawPerson | null {
+    const matchCell = this.sheet
+      .getRange("A:A")
+      .createTextFinder(id)
+      .findNext();
+    if (!matchCell) return null;
+
+    const rowIndex = matchCell.getRowIndex();
+    const row = this.sheet.getRange(`${rowIndex}:${rowIndex}`);
+    return this.buildData(row.getValues()[0]);
+  }
+
+  public buildData(data: string[]): RawPerson {
+    const [id, name, description, link] = data;
+
+    return { id, name, description, link };
+  }
+}
